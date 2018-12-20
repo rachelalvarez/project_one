e6e71865b40976bf581ef29774cd89d8d15713af
Rachel Alvarez <rmaaay45@gmail.com>
2018-12-20 17:55:36 -0600

    created pinghost feature which allows user to ping their local host

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,8 @@
 from src.features.mainmenu import *
+from src.features.pinghost import *
 
 mainmenu.menu()
+userinput = input()
+
+if userinput == "1":
+    pinghost.pinghost()
---
+++ src/features/pinghost/__init__.py
@@ -0,0 +1,1 @@
+from src.features.pinghost import pinghost
