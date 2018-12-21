aa48dcf88a50e7824e657c6282d879807fc17d71
Rachel Alvarez <rmaaay45@gmail.com>
2018-12-20 19:08:56 -0600

    modified pinghost function to catch subprocess error, and to return a statement that the host is down and unhealthy

--- src/features/pinghost/pinghost.py
+++ src/features/pinghost/pinghost.py
@@ -1,7 +1,12 @@
-import socket, subprocess
+import socket, subprocess, os
 
 
 def ping():
-    userip = socket.gethostbyname(socket.gethostname()) #returns the local host's IP address
-    results = subprocess.check_output(["ping", "-c", "1", userip])
-    print(results)
+    try:
+        userip = socket.gethostbyname(socket.gethostname()) #returns the local host's IP address
+        results = subprocess.check_output(["ping", "-c", "1", userip])
+        print("Host Results: " + str(results))
+        print("")
+        print("Host is UP & HEALTHY")
+    except subprocess.CalledProcessError as e:
+        print("Host is DOWN & UNHEALTHY")
