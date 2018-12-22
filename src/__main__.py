3a9cfea1f73181feba8d5af150ba1b73dd78370c
Rachel Alvarez <rmaaay45@gmail.com>
2018-12-21 22:41:46 -0600

    Minor changes to readme file

--- src/__main__.py
+++ src/__main__.py
@@ -9,6 +9,9 @@ while True:
         print("")
         print("Would you like to select another command? If not, type 'exit'. To list available commands, type 'commands'")
 
+    def pleasewait():
+        print("Please wait...")
+
     if userinput == 'exit':
         break
 
@@ -17,22 +20,32 @@ while True:
 
     if userinput == "1": # command: Ping current host
         from src.features.pinghost import *
+        pleasewait()
         pinghost.ping()
         returnstatement()
 
     if userinput == "2": #command: Run netstat on current host
         from src.features.netstathost import *
+        pleasewait()
         netstathost.netstat()
         returnstatement()
 
     if userinput == "3": #command: Run ARP on current host
         from src.features.arphost import *
+        pleasewait()
         arphost.arp()
         returnstatement()
 
     if userinput == "4": #command: Run tracert to specific host
         from src.features.tracerthost import *
-        print("What host would you like to perform trace route on?")
+        print("What host would you like to perform trace route on? (please enter IP address or hostname.com)")
         tracertans = input()
+        pleasewait()
         str(tracerthost.tracert(tracertans))
+        returnstatement()
+
+    if userinput == "5": #command: Run ipconfig on current host
+        from src.features.ipconfighost import *
+        pleasewait()
+        ipconfighost.ipconfig()
         returnstatement()
